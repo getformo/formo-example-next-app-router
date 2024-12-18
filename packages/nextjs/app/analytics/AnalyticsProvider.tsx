fe006c7a755c3cc3a445374701f5f7a322e3eec2
kiuthy229 <nguynmaikiuthy@gmail.com>
2024-12-18 17:30:48 +0700

    add options

--- packages/nextjs/app/analytics/AnalyticsProvider.tsx
+++ packages/nextjs/app/analytics/AnalyticsProvider.tsx
@@ -4,8 +4,12 @@ import React, { FC } from "react";
 import { FormoAnalyticsProvider, FormoAnalyticsProviderProps } from "@formo/analytics";
 
 // The provider component
-export const AnalyticsProvider: FC<FormoAnalyticsProviderProps> = ({ apiKey, children }) => {
-  return <FormoAnalyticsProvider apiKey={apiKey}>{children}</FormoAnalyticsProvider>;
+export const AnalyticsProvider: FC<FormoAnalyticsProviderProps> = ({ apiKey, options, children }) => {
+  return (
+    <FormoAnalyticsProvider apiKey={apiKey} options={options}>
+      {children}
+    </FormoAnalyticsProvider>
+  );
 };
 
 export default AnalyticsProvider;
