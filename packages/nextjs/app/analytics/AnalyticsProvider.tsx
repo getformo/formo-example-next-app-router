697e32566b6f0149c81a8ac3364f558d59ca6e57
Yos Riady <yos@formo.so>
2025-07-15 12:13:30 +0700

    refactor

--- packages/nextjs/app/analytics/AnalyticsProvider.tsx
+++ packages/nextjs/app/analytics/AnalyticsProvider.tsx
@@ -3,9 +3,13 @@
 import React, { FC } from "react";
 import { FormoAnalyticsProvider, FormoAnalyticsProviderProps } from "@formo/analytics";
 
-// The provider component that only runs on the client side
-export const AnalyticsProvider: FC<FormoAnalyticsProviderProps> = props => {
-  return <FormoAnalyticsProvider {...props} />;
+// The provider component
+export const AnalyticsProvider: FC<FormoAnalyticsProviderProps> = ({ writeKey, options, children, disabled }) => {
+  return (
+    <FormoAnalyticsProvider writeKey={writeKey} options={options} disabled={disabled}>
+      {children}
+    </FormoAnalyticsProvider>
+  );
 };
 
 export default AnalyticsProvider;
--- packages/nextjs/app/analytics/index.ts
+++ packages/nextjs/app/analytics/index.ts
@@ -1,2 +1,1 @@
 export { default as AnalyticsProvider } from "./AnalyticsProvider";
-export type { FormoAnalyticsProviderProps } from "@formo/analytics";
