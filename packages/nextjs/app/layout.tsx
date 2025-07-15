1a573c2b1ac9f247fe5f73dc934bf026437e1804
Yos Riady <yos@formo.so>
2025-07-15 13:09:26 +0700

    use client

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -1,14 +1,17 @@
+"use client";
+
 import { AnalyticsProvider } from "./analytics";
 import "@rainbow-me/rainbowkit/styles.css";
 import { ScaffoldEthAppWithProviders } from "~~/components/ScaffoldEthAppWithProviders";
 import { ThemeProvider } from "~~/components/ThemeProvider";
 import "~~/styles/globals.css";
-import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
-export const metadata = getMetadata({
-  title: "Scaffold-ETH 2 App",
-  description: "Built with 🏗 Scaffold-ETH 2",
-});
+// import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
+
+// export const metadata = getMetadata({
+//   title: "Scaffold-ETH 2 App",
+//   description: "Built with 🏗 Scaffold-ETH 2",
+// });
 
 const WRITE_KEY = process.env.FORMO_ANALYTICS_WRITE_KEY || "";
 
